ff42337239fa916869e0dd5280dfe29e7bb75d9f
BehroozRezvani <Brezvani@outlook.com>
2024-07-23 06:57:04 +0100

    inifile class

--- Program.cs
+++ Program.cs
@@ -4,237 +4,173 @@ using System.IO.Compression;
 using System.Runtime.InteropServices;
 using System.Text;
 
-public partial class Zippy
+namespace FolderSaver
 {
-    [DllImport("kernel32.dll", CharSet = CharSet.Ansi, SetLastError = true)]
-    internal static extern uint GlobalAddAtomA(string lpString);
-
-    [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
-    internal static extern ushort GlobalDeleteAtom(uint nAtom);
-
-    [DllImport("user32.dll", SetLastError = true)]
-    private static extern bool RegisterHotKey(IntPtr hWnd, uint keyId, uint fsModifiers, Keys vk);
-
-    [DllImport("user32.dll", SetLastError = true)]
-    private static extern bool UnregisterHotKey(IntPtr hWnd, uint id);
-    [DllImport("user32.dll")]
-    static extern int GetMessageA(out MSG lpMsg, IntPtr hWnd, uint wMsgFilterMin, uint wMsgFilterMax);
-
-    [StructLayout(LayoutKind.Sequential)]
-    public struct POINT
+    public partial class Zippy
     {
-        public int X;
-        public int Y;
-    }
+        [DllImport("kernel32.dll", CharSet = CharSet.Ansi, SetLastError = true)]
+        internal static extern uint GlobalAddAtomA(string lpString);
 
-    [StructLayout(LayoutKind.Sequential)]
-    public struct MSG
-    {
-        IntPtr hwnd;
-        public uint message;
-        public UIntPtr wParam;
-        IntPtr lParam;
-        int time;
-        POINT pt;
-        int lPrivate;
-    }
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        internal static extern ushort GlobalDeleteAtom(uint nAtom);
 
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool RegisterHotKey(IntPtr hWnd, uint keyId, uint fsModifiers, Keys vk);
 
-    const uint MOD_CONTROL = 0x0002;
-    const uint MOD_SHIFT = 0x0004;
-    const uint WM_HOTKEY = 0x0312;
-    static INIFile? iniFile;
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, uint id);
+        [DllImport("user32.dll")]
+        static extern int GetMessageA(out MSG lpMsg, IntPtr hWnd, uint wMsgFilterMin, uint wMsgFilterMax);
 
-    static void loadINIFile(string file)
-    {
-        if (!File.Exists(file))
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
         {
-            Console.WriteLine("INI file not found, defualt shortcuts will be used.\n");
-            Console.WriteLine("Save: CTRL + SHIFT + S");
-            Console.WriteLine("Restore: CTRL + SHIFT + Z");
-            Console.WriteLine("Quit: CTRL + SHIFT + Q");
-            return;
+            public int X;
+            public int Y;
         }
-        iniFile = new INIFile(file);
-        string save = iniFile.Read("Shortcuts", "Save");
-        string restore = iniFile.Read("Shortcut", "Restore");
-        string quit = iniFile.Read("Shortcut", "Quit");
 
-    }
-
-    public static void Main()
-    {
-        Console.WriteLine(System.AppContext.BaseDirectory);
-        static void ZipFolder()
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MSG
         {
-            try
+            readonly IntPtr hwnd;
+            public uint message;
+            public UIntPtr wParam;
+            readonly IntPtr lParam;
+            readonly int time;
+            readonly POINT pt;
+            readonly int lPrivate;
+        }
+
+        const uint MOD_CONTROL = 0x0002;
+        const uint MOD_SHIFT = 0x0004;
+        const uint WM_HOTKEY = 0x0312;
+        
+        public static void Main()
+        {
+            Console.WriteLine(System.AppContext.BaseDirectory);
+            static void ZipFolder()
             {
-                string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-                string zipFolder = Path.Combine(documentsPath, "NBGI_Backup");
-                string sourceFolder = Path.Combine(documentsPath, "NBGI");
-                string zipFile = Path.Combine(zipFolder, $"NBGI_{DateTime.Now:yyyyMMddHHmmss}.zip");
-                if (!Directory.Exists(zipFolder))
+                try
                 {
-                    Directory.CreateDirectory(zipFolder);
-                }
-                using (ZipArchive zip = ZipFile.Open(zipFile, ZipArchiveMode.Create))
-                {
-                    foreach (string file in Directory.EnumerateFiles(sourceFolder, "*", SearchOption.AllDirectories))
+                    string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                    string zipFolder = Path.Combine(documentsPath, "NBGI_Backup");
+                    string sourceFolder = Path.Combine(documentsPath, "NBGI");
+                    string zipFile = Path.Combine(zipFolder, $"NBGI_{DateTime.Now:yyyyMMddHHmmss}.zip");
+                    if (!Directory.Exists(zipFolder))
                     {
-                        zip.CreateEntryFromFile(file, Path.GetRelativePath(sourceFolder, file));
+                        Directory.CreateDirectory(zipFolder);
+                    }
+                    using (ZipArchive zip = ZipFile.Open(zipFile, ZipArchiveMode.Create))
+                    {
+                        foreach (string file in Directory.EnumerateFiles(sourceFolder, "*", SearchOption.AllDirectories))
+                        {
+                            zip.CreateEntryFromFile(file, Path.GetRelativePath(sourceFolder, file));
+                        }
                     }
                 }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
-
-        static void RestoreFolder()
-        {
-            try
-            {
-                string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-                string zipFolder = Path.Combine(documentsPath, "NBGI_Backup");
-                string sourceFolder = Path.Combine(documentsPath, "NBGI");
-
-                string zipFile = "";
-                foreach (string file in Directory.EnumerateFiles(zipFolder))
+                catch (Exception ex)
                 {
-                    if (!file.EndsWith(".zip"))
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            static void RestoreFolder()
+            {
+                try
+                {
+                    string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                    string zipFolder = Path.Combine(documentsPath, "NBGI_Backup");
+                    string sourceFolder = Path.Combine(documentsPath, "NBGI");
+
+                    string zipFile = "";
+                    foreach (string file in Directory.EnumerateFiles(zipFolder))
                     {
-                        continue;
+                        if (!file.EndsWith(".zip"))
+                        {
+                            continue;
+                        }
+                        if (zipFile == "")
+                        {
+                            zipFile = file;
+                        }
+                        if (String.Compare(zipFile, file) < 0)
+                        {
+                            zipFile = file;
+                        }
                     }
                     if (zipFile == "")
                     {
-                        zipFile = file;
+                        Console.WriteLine("No backup file found");
+                        //NotifyIcon notifyIcon = new NotifyIcon();
+                        //notifyIcon.Icon = System.Drawing.SystemIcons.Warning;
+                        //notifyIcon.Visible = true;
+                        //notifyIcon.ShowBalloonTip(5000, "Completed", "This is a toast notification", ToolTipIcon.None);
+                        return;
                     }
-                    if (String.Compare(zipFile, file) < 0)
-                    {
-                        zipFile = file;
-                    }
+                    Console.WriteLine($"Restoring from {zipFile}");
+                    ZipArchive zip = ZipFile.OpenRead(zipFile);
+                    ZipFile.ExtractToDirectory(zipFile, sourceFolder, true);
                 }
-                if (zipFile == "")
+                catch (Exception ex)
                 {
-                    Console.WriteLine("No backup file found");
-                    //NotifyIcon notifyIcon = new NotifyIcon();
-                    //notifyIcon.Icon = System.Drawing.SystemIcons.Warning;
-                    //notifyIcon.Visible = true;
-                    //notifyIcon.ShowBalloonTip(5000, "Completed", "This is a toast notification", ToolTipIcon.None);
-                    return;
+                    Console.WriteLine(ex.Message);
                 }
-                Console.WriteLine($"Restoring from {zipFile}");
-                ZipArchive zip = ZipFile.OpenRead(zipFile);
-                ZipFile.ExtractToDirectory(zipFile, sourceFolder, true);
             }
-            catch (Exception ex)
+
+            // static void Modifiers()
+            // {
+            // }
+
+            uint atom = GlobalAddAtomA("ZipperHotKey");
+            uint atomRestore = GlobalAddAtomA("ZipperHotKeyRestore");
+            uint atomQuit = GlobalAddAtomA("ZipperHotKeyQuit");
+
+            bool Save = RegisterHotKey(0, atom, MOD_CONTROL | MOD_SHIFT, Keys.S);
+            bool Restore = RegisterHotKey(0, atomRestore, MOD_CONTROL | MOD_SHIFT, Keys.Z);
+            bool Quit = RegisterHotKey(0, atomQuit, MOD_CONTROL | MOD_SHIFT, Keys.Q);
+
+            MSG msg;
+            while (GetMessageA(out msg, IntPtr.Zero, 0, 0) != 0)
             {
-                Console.WriteLine(ex.Message);
+                if (msg.message != WM_HOTKEY) continue;
+                System.Console.WriteLine("Hotkey pressed");
+                if (msg.wParam == atom)
+                {
+                    ZipFolder();
+                }
+                else if (msg.wParam == atomRestore)
+                {
+                    RestoreFolder();
+                }
+                else if (msg.wParam == atomQuit)
+                {
+                    break;
+                }
             }
+
+            UnregisterHotKey(0, atom);
+            UnregisterHotKey(0, atomRestore);
+            UnregisterHotKey(0, atomQuit);
+
+            GlobalDeleteAtom(atom);
+            GlobalDeleteAtom(atomRestore);
+            GlobalDeleteAtom(atomQuit);
         }
-
-        // static void Modifiers()
-        // {
-        // }
-
-        uint atom = GlobalAddAtomA("ZipperHotKey");
-        uint atomRestore = GlobalAddAtomA("ZipperHotKeyRestore");
-        uint atomQuit = GlobalAddAtomA("ZipperHotKeyQuit");
-
-        bool Save = RegisterHotKey(0, atom, MOD_CONTROL | MOD_SHIFT, Keys.S);
-        bool Restore = RegisterHotKey(0, atomRestore, MOD_CONTROL | MOD_SHIFT, Keys.Z);
-        bool Quit = RegisterHotKey(0, atomQuit, MOD_CONTROL | MOD_SHIFT, Keys.Q);
-
-        MSG msg;
-        while (GetMessageA(out msg, IntPtr.Zero, 0, 0) != 0)
+        static IniFile? iniFile;
+        static void loadINIFile(string file)
         {
-            if (msg.message != WM_HOTKEY) continue;
-            System.Console.WriteLine("Hotkey pressed");
-            if (msg.wParam == atom)
+            if (!File.Exists(file))
             {
-                ZipFolder();
+                Console.WriteLine("INI file not found, defualt shortcuts will be used.\n");
+                Console.WriteLine("Save: CTRL + SHIFT + S");
+                Console.WriteLine("Restore: CTRL + SHIFT + Z");
+                Console.WriteLine("Quit: CTRL + SHIFT + Q");
+                return;
             }
-            else if (msg.wParam == atomRestore)
-            {
-                RestoreFolder();
-            }
-            else if (msg.wParam == atomQuit)
-            {
-                break;
-            }
+            iniFile = new IniFile(file);
+            string save = iniFile.Read("Shortcuts", "Save");
+            string restore = iniFile.Read("Shortcut", "Restore");
+            string quit = iniFile.Read("Shortcut", "Quit");
         }
-
-        UnregisterHotKey(0, atom);
-        UnregisterHotKey(0, atomRestore);
-        UnregisterHotKey(0, atomQuit);
-
-        GlobalDeleteAtom(atom);
-        GlobalDeleteAtom(atomRestore);
-        GlobalDeleteAtom(atomQuit);
-    }
-}
-
-class INIFile
-{
-    private string filePath;
-
-    [DllImport("kernel32")]
-    private static extern long WritePrivateProfileString(string section,
-    string key,
-    string val,
-    string filePath);
-
-    [DllImport("kernel32")]
-    private static extern int GetPrivateProfileString(string section,
-    string key,
-    string def,
-    StringBuilder retVal,
-    int size,
-    string filePath);
-
-    [DllImport("kernel32")]
-    private static extern int GetPrivateProfileString(int Section,
-    string Key,
-    string Value,
-    [MarshalAs(UnmanagedType.LPArray)] byte[] Result,
-    int Size, string FileName);
-
-    public INIFile(string filePath)
-    {
-        this.filePath = filePath;
-    }
-
-    // public void GetAllPair(INIFile iniFile)
-    // {
-    //     IniFile ini = new IniFile();
-    //     ini.Load("path to your .ini file ...");
-    //     foreach (IniSection section in ini.Sections)
-    //     {
-    //         foreach (IniKey key in section.Keys)
-    //         {
-    //             string sectionName = section.Name;
-    //             string keyName = key.Name;
-    //             string keyValue = key.Value;
-    //         }
-    //     }
-    // }
-
-    public void Write(string section, string key, string value)
-    {
-        WritePrivateProfileString(section, key, value.ToLower(), this.filePath);
-    }
-
-    public string Read(string section, string key)
-    {
-        StringBuilder SB = new StringBuilder(255);
-        int i = GetPrivateProfileString(section, key, "", SB, 255, this.filePath);
-        return SB.ToString();
-    }
-
-    public string FilePath
-    {
-        get { return this.filePath; }
-        set { this.filePath = value; }
     }
 }
